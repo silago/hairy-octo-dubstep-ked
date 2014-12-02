bc62962bbf8191141992e89873de3fa0fa843849
Ovchinnikov Alexander <aovchinnikov@2reallife.com>
2014-12-02 22:04:14 +0300

    Положено начала для рендеров блоков. обновлен bower

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,13 +12,31 @@ angular.module('frontendApp')
 
     $scope.init = function(){
         $scope.info = "loaded";
+        $scope.menu = [{'id':1,'title':'Menu Item 1','url':'#','subitems':[{'id':2,'title':'sub','url':'#'}]}];
+        $scope.data = {'center':
+                            {id:0,
+                            type:'bgvideo',
+                            data:{
+                                    'imgsrc':'images/bgwebm.jpg',
+                                    'src'   :'images/bg.webm',
+                                 },
+                            subitems: []
+                     }
+        };
+    };
+        /*
         menuSrvc.request('GET',{},'').success(function(data){
             $scope.menu = data;
         });
-    }    
+        */
+ 
+
+    
+    
+    
  $scope.trash = [];
  $scope.list1 = {'subitems':[]};
-$scope.list2 = [];
+ $scope.list2 = [];
     $scope.saveMenu = function(data){
         menuSrvc.request('POST',angular.toJson(data),'').success(function(data){
             //$scope.menu = data;
---
+++ app/scripts/directives/block.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name keddoApp.directive:block
+ * @description
+ * # block
+ */
+angular.module('keddoApp')
+  .directive('block', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the block directive');
+      }
+    };
+  });
