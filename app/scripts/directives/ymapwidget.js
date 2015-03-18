18ea71a4990343b0e912f694c6481f92243009b3
Ovchinnikov Alexander <aovchinnikov@2reallife.com>
2015-03-18 23:06:04 +0300

    boss update

--- app/scripts/directives/ymapwidget.js
+++ app/scripts/directives/ymapwidget.js
@@ -9,12 +9,13 @@
 angular.module('frontendApp')
   .directive('ymapwidget', function () {
     return {
-      templateUrl: '/renderers/ymapwidget.html',
+      templateUrl: 'views/renderers/ymapwidget.html',
       scope:true,
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
+//          alert(1);
         
-        element.text('this is the ymapwidget directive');
+      //  element.text('this is the ymapwidget directive');
       }
     };
   });
