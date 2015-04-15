ea5df5866540326238c17a562c1ccbc6e02944c1
silago <silago.nevermind@gmail.com>
2015-04-15 14:45:01 +0800

    new catalog

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -8,7 +8,9 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp') 
-  .controller('BlogCtrl', function ($scope,blogRes,$stateParams,$state) {
+  .controller('BlogCtrl', function ($scope,blogRes,$stateParams,$state,$window) {
         $scope.categories = blogRes.categories({'categories':'*'});
         $scope.page = blogRes.GET($stateParams); 
+        $scope.window = $window;
+        console.log('!!!!!');
   });
